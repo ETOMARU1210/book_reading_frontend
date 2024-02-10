b4d8cd9b4a256720c7b7608d351b9f74387b164d
ETOMARU1210 <149168787+ETOMARU1210@users.noreply.github.com>
2024-02-10 21:43:35 +0900

    image Home.jsx correction

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -122,7 +122,7 @@ const Home = () => {
             <Card>
               <CardMedia
                 component="img"
-                image={"/images/registration.jpg"}
+                image={"/images/registration.png"}
                 alt="本の登録画像"
                 style={{ maxWidth: "58%", height: "auto" }}
               />
