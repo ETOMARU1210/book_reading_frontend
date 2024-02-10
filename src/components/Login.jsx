6e0f1828dd4f70a8b48d40bac32197aa956a60a1
ETOMARU1210 <149168787+ETOMARU1210@users.noreply.github.com>
2024-02-10 22:02:08 +0900

    SnackbarContent maxHeight: 100% add

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
 
   return (
     <Container>
-      {loginerrorMsg && <SnackbarContent message={loginerrorMsg} style={{maxWidth: "100%"}} />}
+      {loginerrorMsg && <SnackbarContent message={loginerrorMsg} style={{maxHeight: "100%"}} />}
       <LoginCard>
         <Typography variant="h5" gutterBottom>
           ログイン
